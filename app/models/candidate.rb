c72ef547d5872df8aee44048f3193158622bbcaf
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-04-01 21:28:47 +0900

    リクエスト一覧の作成、一部のモデルの関連付け、bootstrap3の導入

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,4 @@
 class Candidate < ActiveRecord::Base
+  belongs_to :request, foreign_key: :request_id, class_name: Request
+  belongs_to :member, foreign_key: :member_id, class_name: Member
 end
--- app/models/lightningtalk.rb
+++ app/models/lightningtalk.rb
@@ -1,2 +1,5 @@
 class Lightningtalk < ActiveRecord::Base
+  belongs_to :member, foreign_key: :member_id, class_name: Member
+  has_many :lt_comments
+  has_many :lt_preferences
 end
