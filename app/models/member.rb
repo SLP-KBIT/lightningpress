03a43826d6f3202cb39a355aad5e440c4fc1463c
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-04-01 21:28:47 +0900

    リクエスト一覧の作成、一部のモデルの関連付け、bootstrap3の導入

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,10 @@
 class Member < ActiveRecord::Base
+  has_many :lightningtalks
+  has_many :lt_comments
+  has_many :request_comments
+  has_many :requests
+  has_many :candidates
+  has_many :lt_preferences
+  has_many :request_preferences
+  has_many :request_notifications
 end
