8cc147d9b5a2fced7fa195a7c8af0072cadf9784
joniyjoniy <s13t264@stmail.eng.kagawa-u.ac.jp>
2014-04-02 02:27:49 +0900

    gem event_calendar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140313113843) do
+ActiveRecord::Schema.define(version: 20140401161102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20140313113843) do
   create_table "candidates", force: true do |t|
     t.integer  "request_id"
     t.integer  "member_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.text     "name"
+    t.integer  "member_id"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.text     "content_path"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
