b7971e730d5ee7feef29f304247fa4316b90ae4e
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-04-04 03:06:30 +0900

    リクエスト新規作成ウィンドウの作成

--- app/controllers/lt_requests_controller.rb
+++ app/controllers/lt_requests_controller.rb
@@ -1,5 +1,15 @@
 class LtRequestsController < ApplicationController
   def index
     @requests = Request.all
-  end 
+    @request_new = Request.new
+    @member = Member.all
+  end
+  def create
+    @request = request.new(request_params)
+    @request.save
+    redirect_to requests_path
+  end
+  def requests_params
+    params.require(:request).permit(:title, :content, :presenter_id)
+  end
 end
