042923f1ebba62d82b5ad434371a12960c904825
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-04-04 23:59:06 +0900

    リクエスト新規作成ウィンドウのデザイン変更 ユーザ認証機能の実装   ・ログイン、ログアウト   ・ログインしていない状態で他のページにアクセスすると、ログインページにリダイレクト 　・パスワードの暗号化   ・ログインユーザのIDをどこからでも参照できるように NavigationBarの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_member
+
+  def current_member
+    if session[:member_id]
+      @current_member ||= Member.where(id: session[:member_id]).first
+    end
+  end
+
+  helper_method :current_member
 end
--- app/controllers/lt_requests_controller.rb
+++ app/controllers/lt_requests_controller.rb
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 class LtRequestsController < ApplicationController
+  before_action :sample_filter
   def index
     @requests = Request.all
     @request_new = Request.new
@@ -6,10 +8,17 @@ class LtRequestsController < ApplicationController
   end
   def create
     @request = Request.new(requests_params)
+    @request.presenter_id? ? @request.status = 1 : @request.status = 0
     @request.save
     redirect_to lt_requests_path
   end
   def requests_params
     params.require(:request).permit(:title, :content, :presenter_id, :contributor_id)
+  end
+
+  private
+
+  def sample_filter
+    redirect_to logins_path unless session[:member_id]
   end
 end
