919badc07d680e7c0fc502185dfaef5f6639011d
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-04-04 23:59:06 +0900

    リクエスト新規作成ウィンドウのデザイン変更 ユーザ認証機能の実装   ・ログイン、ログアウト   ・ログインしていない状態で他のページにアクセスすると、ログインページにリダイレクト 　・パスワードの暗号化   ・ログインユーザのIDをどこからでも参照できるように NavigationBarの作成

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: candidates
+#
+#  id         :integer          not null, primary key
+#  request_id :integer
+#  member_id  :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Candidate < ActiveRecord::Base
   belongs_to :request, foreign_key: :request_id, class_name: Request
   belongs_to :member, foreign_key: :member_id, class_name: Member
--- app/models/request_preference.rb
+++ app/models/request_preference.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: request_preferences
+#
+#  id               :integer          not null, primary key
+#  lightningtalk_id :integer
+#  grade            :integer
+#  member_id        :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class RequestPreference < ActiveRecord::Base
 end
