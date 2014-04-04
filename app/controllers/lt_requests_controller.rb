ad88338a24bb0c636cb1237f68ef8adf4a985d6d
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-04-04 15:08:34 +0900

    リクエスト新規作成機能作成

--- app/controllers/lt_requests_controller.rb
+++ app/controllers/lt_requests_controller.rb
@@ -5,11 +5,11 @@ class LtRequestsController < ApplicationController
     @member = Member.all
   end
   def create
-    @request = request.new(request_params)
+    @request = Request.new(requests_params)
     @request.save
-    redirect_to requests_path
+    redirect_to lt_requests_path
   end
   def requests_params
-    params.require(:request).permit(:title, :content, :presenter_id)
+    params.require(:request).permit(:title, :content, :presenter_id, :contributor_id)
   end
 end
