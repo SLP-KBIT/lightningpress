b6762297e3c72686fd017ced26386ee8802f0dbf
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-04-04 23:59:06 +0900

    リクエスト新規作成ウィンドウのデザイン変更 ユーザ認証機能の実装   ・ログイン、ログアウト   ・ログインしていない状態で他のページにアクセスすると、ログインページにリダイレクト 　・パスワードの暗号化   ・ログインユーザのIDをどこからでも参照できるように NavigationBarの作成

--- app/models/lt_comment.rb
+++ app/models/lt_comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: lt_comments
+#
+#  id               :integer          not null, primary key
+#  member_id        :integer
+#  content          :text
+#  created_at       :datetime
+#  lightningtalk_id :integer
+#  updated_at       :datetime
+#
+
 class LtComment < ActiveRecord::Base
   belongs_to :member, foreign_key: :member_id, class_name: Member
   belongs_to :lightningtalk, foreign_key: :lightningtalk_id, class_name: Lightningtalk
