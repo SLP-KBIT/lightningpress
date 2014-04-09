078840ae6325498b46e57a01c95bd5fa73341513
ookawa <clc.iguto@gmail.com>
2014-04-09 17:42:39 +0900

    リファクタリング
      Viewでモデルを取得している箇所をコントローラで取得し、インスタンス変数経由で渡すように修正
      マジックナンバーを定数化

--- app/models/request.rb
+++ app/models/request.rb
@@ -19,4 +19,17 @@ class Request < ActiveRecord::Base
   has_many :candidates
   has_many :request_preferences
   has_one :request_notification
+
+  module Status
+    None     = 0
+    Waiting  = 1
+    Assigned = 2
+  end
+
+  def status_name
+    return "担当者無し" if status == Status::None
+    return "承認待ち" if status == Status::Waiting
+    return "承認済み" if status == Status::Assigned
+  end
+
 end
