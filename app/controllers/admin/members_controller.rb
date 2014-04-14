4178d95a92fa11b18811f6134e66b5d4e871f18d
ookawa <clc.iguto@gmail.com>
2014-04-14 20:59:15 +0900

    メンバー管理 微調整

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -3,7 +3,7 @@ class Admin::MembersController < ApplicationController
   before_action :get_member
 
   def index
-    @members = Member.all
+    @members = Member.order(:id).all
   end
 
   def new
