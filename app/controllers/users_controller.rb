f82a03c5001ccd1bfd2ab9b2d42503a855495172
iguto <clc.iguto@gmail.com>
2014-04-21 22:48:15 +0900

    立候補関連のCSS記述の配置を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,14 @@ class UsersController < ApplicationController
   def top
     candidate_election
   end
-  
+
+  private
+
   def candidate_election
-    p session[:member_id]
     @recruiting_requests = Request.where(contributor_id: session[:member_id], presenter_id: nil)
     @candidates = {}
     @recruiting_requests.each_with_index do |request,i|
       @candidates.store(request.id ,Candidate.where(request_id: request.id))
     end
-
   end
-
 end
