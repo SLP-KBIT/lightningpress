4a2c8cbf97c52ddcfbd53bc08259c0c260ae4e61
Mizobuchi Hisanori <s11t275@stmail.eng.kagawa-u.ac.jp>
2014-04-23 18:39:17 +0900

    立候補した人を選出する機能を追加

--- app/controllers/lt_requests_controller.rb
+++ app/controllers/lt_requests_controller.rb
@@ -5,12 +5,28 @@ class LtRequestsController < ApplicationController
     @request = Request.new
     @members = Member.all
   end
-  
+
   def create
     @request = Request.new(requests_params)
     @request.presenter_id? ? @request.status = Request::Status::Waiting : @request.status = Request::Status::None
     @request.contributor_id = @current_member.id
     @request.save
+    redirect_to lt_requests_path
+  end
+
+  def update
+    @request = Request.where(id: params[:id])
+    @request.attributes = params.require(:requests).permit(:id, :contributor_id, :title,
+            :content, :presenter_id, :status)
+    @request.save
+    redirect_to lt_requests_path
+  end
+
+  def request_to
+    @request = Request.where(id: params[:id]).first
+    @request.presenter_id = params[:presenter_id]
+    @request.status = Request::Status::Assigned
+    @request.save!
     redirect_to lt_requests_path
   end
 
@@ -30,4 +46,5 @@ class LtRequestsController < ApplicationController
       :contributor_id
     )
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,9 @@ class UsersController < ApplicationController
   def top
     candidate_election
   end
-  
+
   def candidate_election
-    p session[:member_id]
-    @recruiting_requests = Request.where(contributor_id: session[:member_id], presenter_id: nil)
+    @recruiting_requests = Request.where(contributor_id: session[:member_id], status: Request::Status::None)
     @candidates = {}
     @recruiting_requests.each_with_index do |request,i|
       @candidates.store(request.id ,Candidate.where(request_id: request.id))
