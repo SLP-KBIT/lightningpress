a63a471edbc9d6f562f916c0817728ac03d8c8f4
Mizobuchi Hisanori <s11t275@stmail.eng.kagawa-u.ac.jp>
2014-04-23 18:39:17 +0900

    立候補した人を選出する機能を追加

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,10 +6,15 @@ class CandidatesController < ApplicationController
     redirect_to lt_requests_path
   end
 
+  def choose
+    @candidate = Candidate.where(id: params[:id]).first
+    redirect_to :controller => 'lt_requests', :action => 'request_to', :id => @candidate.request_id, :presenter_id => @candidate.member_id
+  end
+
   def candidates_params
     params.require(:candidate).permit(
       :request_id,
-      :member_id      
+      :member_id
     )
   end
 
