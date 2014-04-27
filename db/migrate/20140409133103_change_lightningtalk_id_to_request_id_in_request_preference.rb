25300bccd01275160b47bfe2d383c2fd73ab47c4
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-04-27 21:51:48 +0900

    lightningtalkテーブルのmember_idをinteger型に修正

--- db/migrate/20140409133103_change_lightningtalk_id_to_request_id_in_request_preference.rb
+++ db/migrate/20140409133103_change_lightningtalk_id_to_request_id_in_request_preference.rb
@@ -2,4 +2,8 @@ class ChangeLightningtalkIdToRequestIdInRequestPreference < ActiveRecord::Migrat
   def self.up
     rename_column :request_preferences, :lightningtalk_id, :request_id
   end
+
+  def self.down
+    rename_column :request_preferences, :request_id, :lightningtalk_id
+  end
 end
