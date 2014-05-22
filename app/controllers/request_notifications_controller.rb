edcae41d3cfa57656d89d8f44b862339a633983b
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-05-22 18:21:36 +0900

    LT依頼を受諾時に、lightningtalkを作成する

--- app/controllers/request_notifications_controller.rb
+++ app/controllers/request_notifications_controller.rb
@@ -5,6 +5,7 @@ class RequestNotificationsController < ApplicationController
     if params[:response] == "受諾"
       @request.update_attributes(status: Request::Status::Assigned)
       @request.request_notification.update_attributes(receiver_id: @request.contributor_id, response_status: RequestNotification::ResponseStatus::Accept)
+      @lightningtalk = Lightningtalk.create(name: @request.title, sumally: @request.content, member_id: @request.presenter_id)
     elsif params[:response] == "拒否"
       @request.update_attributes(status: Request::Status::None)
       @request.request_notification.update_attributes(receiver_id: @request.contributor_id, response_status: RequestNotification::ResponseStatus::Refuse)
