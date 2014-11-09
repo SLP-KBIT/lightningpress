9a0a9664d1dc1744165cff312ccbc96189661611
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-11-10 02:34:00 +0900

    lt_requests_contollerのcreateで、presenter_idによる、募集と申請の動作が逆になっていたので修正

--- app/controllers/lt_requests_controller.rb
+++ app/controllers/lt_requests_controller.rb
@@ -10,13 +10,13 @@ class LtRequestsController < ApplicationController
     @request = Request.new(request_params)
     @request.contributor_id = @current_member.id
     if @request.presenter_id
-      @request.status = Request::Status::None
-    else
       @request.status = Request::Status::Waiting
       @notification = @request.build_request_notification(
         receiver_id: @request.presenter_id,
         response_status: RequestNotification::ResponseStatus::Unread
       )
+    else
+      @request.status = Request::Status::None
     end
     @request.save!
     redirect_to lt_requests_path
